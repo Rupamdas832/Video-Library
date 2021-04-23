26a1ef9b399d58dc1fd49dafc97be922b9b63773
Rupam <rupamdas832@gmail.com>
2021-04-23 12:37:11 +0530

    style: minor changes

--- src/Pages/Categories/Categories.js
+++ src/Pages/Categories/Categories.js
@@ -15,7 +15,7 @@ export const Categories = () => {
             {categories.map( category=> {
                 const {categoryId,title} = category
                 return <div className="category" key={categoryId}>
-                        <Link to={`/category/${categoryId}`}><button className="btn outline categories">{title}</button></Link>
+                        <Link to={`/category/${categoryId}`} className="routerLink"><button className="btn outline categories">{title}</button></Link>
                     </div>
             })}
         </div>
